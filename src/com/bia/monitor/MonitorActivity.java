6226ba06fd916f2cc2c9f426dcf2af1d21023d45
Intesar Mohammed <mdshannan@gmail.com>
2012-05-11 19:24:41 -0700

    working android project

---
+++ src/com/bia/monitor/MonitorActivity.java
@@ -0,0 +1,16 @@
+package com.bia.monitor;
+
+import org.apache.cordova.DroidGap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MonitorActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
